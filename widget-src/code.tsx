e3625dc1bf6951ab5e162145549f470513eb99a8
Lovefields <hho1993@gmail.com>
2022-12-15 11:17:26 +0900

    feat: done logic test

--- widget-src/code.tsx
+++ widget-src/code.tsx
@@ -1,82 +1,255 @@
 // This is a counter widget with buttons to increment and decrement the number.
 
 const { widget } = figma;
-const { useSyncedState, usePropertyMenu, AutoLayout, Text, Rectangle, Input, useEffect } = widget;
+const { useWidgetId, useSyncedState, usePropertyMenu, AutoLayout, Text, Rectangle, Input, useEffect } = widget;
 
 function Widget() {
-    const [widgetData, setWidgetData] = useSyncedState("widgetData", "");
+    const [widgetType] = useSyncedState("widgetType", "parent");
+    const [parentId] = useSyncedState("parentId", "");
+    const [dataType] = useSyncedState("dataType", "");
+    const [dataId] = useSyncedState("dataId", "");
+    const [widgetData, setWidgetData] = useSyncedState(
+        "widgetData",
+        JSON.stringify({
+            visible: [],
+            invisible: [],
+            tracking: [],
+            design: [],
+        })
+    );
     const [pageTitle, setPageTitle] = useSyncedState("pageTitle", "");
     const [pageCaption, setPageCaption] = useSyncedState("pageCaption", "");
-    let widgetDataJson = {
-        visible: [{ id: 0, type: 1, content: "test" }],
-        invisible: [],
-        tracking: [],
-    };
+    let widgetDataJson = JSON.parse(widgetData);
+    let widgetId = useWidgetId();
+
+    console.log("Type : ", widgetType);
+    console.log("parent ID : ", parentId);
+    console.log("dataType : ", dataType);
+    console.log("dataId : ", dataId);
+    console.log("widget ID : ", useWidgetId());
 
     useEffect(() => {
         figma.ui.onmessage = (msg) => {
-            if (msg.type === "showToast") {
-                figma.notify("Hello widget");
+            let data = JSON.parse(msg.data);
+
+            if (msg.type === "setting") {
+                console.log("id?", data);
+                setWidgetId(data);
             }
-            if (msg.type === "close") {
-                figma.closePlugin();
+
+            if (msg.type === "add") {
+                widgetDataJson[data.type].push(data);
+                setData();
             }
+
+            if (msg.type === "add") {
+            }
+
+            if (msg.type === "pointer") {
+                const allWidgetNodes: WidgetNode[] = figma.currentPage.findAll((node) => {
+                    return node.type === "WIDGET";
+                });
+
+                const myWidgetNodes: WidgetNode[] = allWidgetNodes.filter((node) => {
+                    return node.widgetId === figma.widgetId;
+                });
+                console.log("make pointer !!!!");
+                console.log(data);
+                console.log(myWidgetNodes);
+
+                myWidgetNodes[0].cloneWidget({
+                    widgetType: "child",
+                    parentId: widgetId,
+                    dataType: data.type,
+                    dataId: data.id,
+                    widgetData: widgetData,
+                });
+            }
+
+            figma.closePlugin();
         };
     });
 
-    usePropertyMenu(
-        [
-            {
-                itemType: "action",
-                propertyName: "add",
-                tooltip: "Add new info box",
-                icon: `<svg width="22" height="15" viewBox="0 0 22 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path fill-rule="evenodd" clip-rule="evenodd" d="M11.9026 1.43168C12.1936 1.47564 12.4822 1.54098 12.7663 1.62777L12.7719 1.62949C14.0176 2.0114 15.109 2.78567 15.8858 3.83854L15.8918 3.84665C16.5473 4.73808 16.9484 5.78867 17.058 6.88508L14.0863 4.88858L13.3259 6.02047L17.3852 8.74774L17.9079 9.09894L18.2994 8.60571L21.0056 5.19662L19.9376 4.34879L18.3531 6.34479C18.3424 6.27511 18.3306 6.20563 18.3179 6.13636C18.1135 5.02233 17.6601 3.96334 16.9851 3.04274L16.9791 3.03462C16.0303 1.74427 14.6956 0.794984 13.1714 0.326388L13.1658 0.32466C12.8171 0.217755 12.4627 0.137298 12.1055 0.0832198C10.899 -0.0994351 9.66061 0.0188515 8.50099 0.435448L8.4947 0.437711C7.42511 0.823053 6.46311 1.44778 5.6774 2.25801C5.38576 2.55876 5.11841 2.88506 4.87886 3.23416C4.85856 3.26376 4.83845 3.29351 4.81854 3.32343L5.94262 4.08294L5.94802 4.07484C5.96253 4.0531 5.97717 4.03146 5.99195 4.00993C6.71697 2.95331 7.75331 2.15199 8.95541 1.72013L8.9617 1.71788C9.33245 1.58514 9.71301 1.48966 10.098 1.43156C10.6957 1.34135 11.3039 1.34123 11.9026 1.43168ZM3.70034 6.39429L0.994141 9.80338L2.06217 10.6512L3.64663 8.65521C3.65741 8.72489 3.66916 8.79437 3.68187 8.86364C3.88627 9.97767 4.33964 11.0367 5.01467 11.9573L5.02063 11.9654C5.96945 13.2557 7.30418 14.205 8.82835 14.6736L8.83398 14.6753C9.18281 14.7823 9.53732 14.8628 9.89464 14.9168C11.101 15.0994 12.3393 14.9811 13.4988 14.5646L13.5051 14.5623C14.5747 14.1769 15.5367 13.5522 16.3224 12.742C16.614 12.4413 16.8813 12.115 17.1209 11.7659C17.1412 11.7363 17.1613 11.7065 17.1812 11.6766L16.0571 10.9171L16.0518 10.9252C16.0372 10.9469 16.0225 10.9686 16.0078 10.9902C15.2827 12.0467 14.2464 12.848 13.0444 13.2799L13.0381 13.2821C12.6673 13.4149 12.2868 13.5103 11.9018 13.5684C11.3041 13.6587 10.6958 13.6588 10.0971 13.5683C9.8062 13.5244 9.51754 13.459 9.23347 13.3722L9.22784 13.3705C7.98212 12.9886 6.89078 12.2143 6.11393 11.1615L6.10795 11.1534C5.45247 10.2619 5.05138 9.21133 4.94181 8.11492L7.91342 10.1114L8.6739 8.97953L4.61459 6.25226L4.09188 5.90106L3.70034 6.39429Z" fill="white"/>
-            </svg>
-            `,
-            },
-        ],
-        () => {
-            return new Promise((resolve) => {
-                figma.showUI(__html__, { width: 300, height: 300 });
-                figma.ui.postMessage(JSON.stringify({ mode: "new" }));
+    // data arrangement
+    function arrangementData(data) {
+        Object.entries(data).map(([key, value]) => {
+            value.forEach((row, i) => {
+                data[key][i].id = i + 1;
             });
+        });
+
+        return data;
+    }
+
+    function setData() {
+        widgetDataJson = arrangementData(widgetDataJson);
+        setWidgetData(JSON.stringify(widgetDataJson));
+    }
+    if (widgetType == "parent") {
+        usePropertyMenu(
+            [
+                {
+                    itemType: "action",
+                    propertyName: "add",
+                    tooltip: "Add New Description",
+                },
+            ],
+            ({ propertyName, propertyValue }) => {
+                if (propertyName == "add") {
+                    return new Promise((resolve) => {
+                        figma.showUI(__html__, { width: 300, height: 300 });
+                        figma.ui.postMessage(
+                            JSON.stringify({
+                                mode: "new",
+                                id: null,
+                                type: "",
+                                content: "",
+                            })
+                        );
+                    });
+                }
+            }
+        );
+    }
+
+    let count = 0;
+    for (let [key, value] of Object.entries(widgetDataJson)) {
+        if (value.length > 0) {
+            count += 1;
         }
-    );
+    }
 
-    return (
-        <AutoLayout name="wrap" direction={"vertical"} spacing={8} padding={16} cornerRadius={8} fill={"#FFFFFF"} stroke={"#E6E6E6"}>
-            <AutoLayout name="Page information" direction={"vertical"}>
-                <Input
-                    value={pageTitle}
-                    placeholder="Page Title"
-                    onTextEditEnd={(e) => {
-                        setPageTitle(e.characters);
-                    }}
-                    fontSize={24}
-                    fontWeight={700}
-                    fill="#333"
-                    width={400}
-                    inputBehavior="multiline"
-                />
-                <Input
-                    value={pageCaption}
-                    placeholder="Page Description"
-                    onTextEditEnd={(e) => {
-                        setPageCaption(e.characters);
-                    }}
-                    fontSize={16}
-                    fill="#333"
-                    width={400}
-                    inputBehavior="multiline"
-                />
-            </AutoLayout>
+    if (count == 0) {
+        count = 1;
+    }
 
-            <AutoLayout name="id" width={20} height={20} horizontalAlignItems={"center"} verticalAlignItems={"center"} padding={2} cornerRadius={50}>
-                <Text fontSize={16} horizontalAlignText={"center"}></Text>
+    let widgetWidth = 400 * count;
+
+    function listStructure() {
+        let column = Object.entries(widgetDataJson).map(([key, value]) => {
+            if (value.length > 0) {
+                let item = value.map((row) => {
+                    return itemStructure(row);
+                });
+
+                return (
+                    <AutoLayout name={`${key}-column`} width={"fill-parent"} direction="vertical" spacing={10} key={key}>
+                        {item}
+                    </AutoLayout>
+                );
+            }
+        });
+
+        return column;
+    }
+
+    function itemStructure(data) {
+        return (
+            <AutoLayout name="item" width={"fill-parent"} direction="vertical" key={data.id}>
+                <AutoLayout name="top-area">
+                    <Text>{data.id}</Text>
+                    <Text>{data.type}</Text>
+                </AutoLayout>
+
+                <Text>{data.content}</Text>
+
+                <AutoLayout name="btn-area">
+                    <Text
+                        onClick={(e) => {
+                            widgetDataJson[data.type].splice(data.id - 1, 1);
+                            setData();
+                        }}
+                    >
+                        Delete
+                    </Text>
+                    <Text
+                        onClick={(e) => {
+                            console.log("Edit", data.id, data.type);
+                        }}
+                    >
+                        Edit
+                    </Text>
+                    <Text
+                        onClick={(e) => {
+                            return new Promise((resolve) => {
+                                figma.showUI(`
+                                    <script>
+                                        window.onmessage = (event) => {
+                                            let data = event.data.pluginMessage;
+                                            parent.postMessage({ pluginMessage: { type: "pointer", data: data } }, "*");
+                                        };
+                                    </script>
+                                `);
+                                figma.ui.postMessage(JSON.stringify(widgetDataJson[data.type][data.id - 1]));
+                            });
+                        }}
+                    >
+                        Make Pointer
+                    </Text>
+                </AutoLayout>
             </AutoLayout>
-        </AutoLayout>
-    );
+        );
+    }
+
+    if (widgetType == "parent") {
+        return (
+            <AutoLayout name="wrap" width={widgetWidth} direction={"vertical"} spacing={8} padding={16} cornerRadius={8} fill={"#FFFFFF"} stroke={"#E6E6E6"}>
+                <AutoLayout name="Page information" width={"fill-parent"} direction={"vertical"}>
+                    <Input
+                        value={pageTitle}
+                        placeholder="Page Title"
+                        onTextEditEnd={(e) => {
+                            setPageTitle(e.characters);
+                        }}
+                        fontSize={24}
+                        fontWeight={700}
+                        width={"fill-parent"}
+                        fill="#333"
+                        inputBehavior="multiline"
+                    />
+                    <Input
+                        value={pageCaption}
+                        placeholder="Page Description"
+                        onTextEditEnd={(e) => {
+                            setPageCaption(e.characters);
+                        }}
+                        fontSize={16}
+                        width={"fill-parent"}
+                        fill="#333"
+                        inputBehavior="multiline"
+                    />
+                </AutoLayout>
+                <AutoLayout name="list-area" width={"fill-parent"} spacing={"auto"}>
+                    {listStructure()}
+                </AutoLayout>
+            </AutoLayout>
+        );
+    } else {
+        return (
+            <AutoLayout
+                name="wrap"
+                width={40}
+                height={40}
+                fill={"#FFFFFF"}
+                stroke={"#E6E6E6"}
+                onClick={(e) => {
+                    return new Promise((resolve) => {
+                        figma.showUI(__html__, { width: 300, height: 300 });
+                        figma.ui.postMessage(
+                            JSON.stringify({
+                                mode: "view",
+                                id: null,
+                                type: "",
+                                content: "",
+                            })
+                        );
+                    });
+                }}
+            >
+                <Text>1</Text>
+            </AutoLayout>
+        );
+    }
 }
 
 widget.register(Widget);
